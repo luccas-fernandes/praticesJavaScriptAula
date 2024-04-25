ecbbc68af0a0a16f515a53d7e4e8e03ec5d98d1b
Gustavo <gstvrrds@gmail.com>
2024-04-25 17:19:03 -0300

    ultima modificacao

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 let nomeDoCliente = prompt('Insira seu nome:');
 
+console.log('Compras de ', nomeDoCliente);
+
 let produto1 = 'Shampoo';
 let marcaProduto1 = 'Seda';
 let precoProduto1 = '7.50';
 
-console.log(nomeDoCliente, ' comprou um ', produto1, 'da marca', marcaProduto1, 'que custa', precoProduto1, )
+console.log(' Produto 1: ', produto1, '\n', 'Marca do produto: ', marcaProduto1, '\n', 'Preço: ', precoProduto1, '\n');
 
 let produto2 = 'Condicionador';
 let marcaProduto2 = 'Pantene';
-let precoProduto2 = '16.25'
-let maisCaro = parseFloat(precoProduto2) > parseFloat(precoProduto1)
+let precoProduto2 = '16.25';
+// let maisCaro = parseFloat(precoProduto2) > parseFloat(precoProduto1)
 
-console.log(maisCaro)
+// console.log(maisCaro)
+
+console.log(' Produto 2: ', produto2, '\n', 'Marca do produto: ', marcaProduto2, '\n', 'Preço: ', precoProduto2, '\n');
+
+let totalCompra = parseFloat(precoProduto1) + parseFloat(precoProduto2);
+
+console.log('Total de compras de', nomeDoCliente, 'é', totalCompra);
+
+const TAXA_IMPOSTO = 0.10 * totalCompra;
+const TAXA_FRETE = 0.15 * totalCompra;
+
+let distancia = prompt('Insira a distância até sua casa: ');
+
+let frete = TAXA_FRETE * parseFloat(distancia);
+let imposto = frete + TAXA_IMPOSTO;
+
+console.log('Frete: ', frete, '\n', 'Imposto: ', imposto);
+
+let totalFinal = totalCompra + frete + imposto
+
+console.log('O preço final da compra é: ', totalFinal.toFixed(2))
 
